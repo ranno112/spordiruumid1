5939d42eebe2c366e5726993df4ae76c84d7932a
Annemarii Hunt <annemarii.hunt@gmail.com>
2020-04-26 14:05:16 +0300

    PArandatud mobiilivaates asutuse lisamine

--- application/views/pages/createBuilding.php
+++ application/views/pages/createBuilding.php
@@ -14,8 +14,8 @@
             <?php echo form_open('building/register'); ?>
 
 				<h4 class="pt-2 txt-xl px-5 mx-5">Asutuse info</h4>
-				<div class="d-flex p-0 mt-4 px-5 mx-5">
-                    <div class="form-label-group col-6 py-0 pl-0 pr-5">
+				<div class="row d-flex p-0 mt-4 px-md-5 px-2 mx-md-5 mx-2">
+                    <div class="form-label-group col-12 col-md-6 py-0 pl-0 pr-md-5">
 										<label for="status">Piirkond</label>
                     <select id="place" name="place" class="form-control arrow">
 										<?php foreach($regions as $region) {?>
@@ -24,23 +24,23 @@
                     </select>
                     </div>
                 </div>
-                <div class="d-flex p-0 mt-4 px-5 mx-5">
-                    <div class="form-label-group col-6 py-0 pl-0 pr-5">
+                <div class="row d-flex p-0 mt-4 px-md-5 px-2 mx-md-5 mx-2">
+                    <div class="form-label-group col-12 col-md-6 py-0 pl-0 pr-md-5">
 						<label>Asutuse nimi*</label>
 						<input class="form-control"  type="text" name="name" required>
                     </div>
-                    <div class="form-label-group col-6 p-0 pl-5">
+                    <div class="form-label-group col-12 col-md-6 py-0 pl-0 pr-md-5">
                         <label>Kontakt email</label>
                         <input type="email" class="form-control" name="email">
                     </div>
                 </div>
 
-                <div class="d-flex p-0 mt-4 px-5 mx-5">
-                    <div class="form-label-group col-6 py-0 pl-0 pr-5">
+                <div class="row d-flex p-0 mt-4 px-md-5 px-2 mx-md-5 mx-2">
+                    <div class="form-label-group col-12 col-md-6 py-0 pl-0 pr-md-5">
                         <label>Telefoni number</label>
                         <input type="number" class="form-control" name="phone">
                     </div>
-                    <div class="form-label-group col-6 p-0 pl-5">
+                    <div class="form-label-group col-12 col-md-6 py-0 pl-0 pr-md-5">
                         <label>Hinnakirja URL</label>
                         <input type="text" class="form-control" name="price_url">
                     </div>
